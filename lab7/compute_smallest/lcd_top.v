146216b848f939c2f76546ce22ac37209a38631f
MohitKS5 <mohitkumarsingh907@gmail.com>
2019-03-11 14:47:51 +0530

    final lab 7 ass 2

--- lab7/compute_smallest/lcd_top.v
+++ lab7/compute_smallest/lcd_top.v
@@ -9,9 +9,9 @@ module lcd_output_top(
 	input PB1,PB2, PB3, PB4,
 	input [3:0] holder
     );
-	reg [3:0] num1,num2,num3,num4,min;
+	reg [3:0] num1,num2,num3,num4;
 	reg [1:0] min_index;
-	wire [3:0] less,greater,equality;
+	wire [5:0] less,greater,equality;
 	always @(posedge PB1) begin
 		num1[3:0] = holder[3:0];
 	end
@@ -40,26 +40,24 @@ module lcd_output_top(
 	n_bit_adder #(8) A4({5'h0,num4}, 8'h30, charset_index_num4);
 	n_bit_adder #(8) A5({6'h0,min_index}, 8'h30, charset_index_output);
 
-	n_bit_comparator #(3) C1(num1,min,less[0],greater[0],equality[0]);
-	n_bit_comparator #(3) C2(num2,min,less[1],greater[1],equality[1]);
-	n_bit_comparator #(3) C3(num3,min,less[2],greater[2],equality[2]);
-	n_bit_comparator #(3) C4(num4,min,less[3],greater[3],equality[3]);
+	n_bit_comparator #(3) C1(num1,num2,less[0],greater[0],equality[0]);
+	n_bit_comparator #(3) C2(num1,num3,less[1],greater[1],equality[1]);
+	n_bit_comparator #(3) C3(num1,num4,less[2],greater[2],equality[2]);
+	n_bit_comparator #(3) C4(num2,num3,less[3],greater[3],equality[3]);
+	n_bit_comparator #(3) C5(num2,num4,less[4],greater[4],equality[4]);
+	n_bit_comparator #(3) C6(num3,num4,less[5],greater[5],equality[5]);
 
 	always @(posedge clk) begin
-		if(less[0]) begin 
-			min <= num1;
+		if(less[0]&less[1]&less[2]) begin 
 			min_index <= 0;
 		end
-		else if(less[1])begin 
-			min <= num2;
+		else if(less[3]&less[4]&greater[0])begin 
 			min_index <= 1;
 		end
-		else if(less[2]) begin 
-			min <= num3;
+		else if(less[5]&greater[3]&greater[1]) begin 
 			min_index <= 2;
 		end
-		else if(less[3]) begin 
-			min <= num4;
+		else if(greater[5]&greater[4]&greater[2]) begin 
 			min_index <= 3;
 		end
 	end
